51024298006f3832144309933c5f6dcd69a6e53b
Edgar <eddyade98@gmail.com>
2023-09-24 21:46:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function countDown(number) {
+    if (typeof number !== 'number' || number <= 0 || !Number.isInteger(number)) {
+      console.log("Submit a valid positive integer.");
+      return;
+    }
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+  }
+
+ function writeCards(names) {
+    const thankYouMessages = [];
+
+    for (let n = 0; n < names.length; n++) {
+      thankYouMessages.push(`Thank you, ${names[n]}, for your kindness!`);
+    }
+    return(thankYouMessages);
+  }
+
